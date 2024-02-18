24a5b103218062d5d152160781976d3f1412b6bb
Ruben-Bellido <bellido.ruben@opendeusto.es>
2024-02-19 00:20:14 +0100

    app v1

--- app/filebeat/generate-data.py
+++ app/filebeat/generate-data.py
@@ -1,12 +1,21 @@
 import json
+from datetime import datetime
+
 
 # Datos dummy para los archivos JSON
-data = {
-  "name": "John",
-  "age": 30,
-  "city": "New York"
-}
+timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+data0 = {"id": 0, "state": "ON", "timestamp": timestamp}
+data1 = {"id": 1, "state": "OFF", "timestamp": timestamp}
+
+timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+data2 = {"id": 2, "state": "OFF", "timestamp": timestamp}
+data3 = {"id": 3, "state": "ON", "timestamp": timestamp}
 
 # Escribir los datos en los archivos JSON
-with open('/usr/share/filebeat/data/dummy.json', 'w') as f:
-    json.dump(data, f)
+with open('/usr/share/filebeat/logs/dummy_0.json', 'w') as f:
+    f.write(json.dumps(data0) + '\n')
+    f.write(json.dumps(data1) + '\n')
+
+with open('/usr/share/filebeat/logs/dummy_1.json', 'w') as f:
+    f.write(json.dumps(data2) + '\n')
+    f.write(json.dumps(data3) + '\n')

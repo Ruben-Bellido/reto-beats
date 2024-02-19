eea5fa1ddddc640c77af2c672d425977fc6d483e
Ruben-Bellido <bellido.ruben@opendeusto.es>
2024-02-19 20:29:30 +0100

    app v2

--- app/filebeat/generate-data.py
+++ app/filebeat/generate-data.py
@@ -1,21 +1,27 @@
 import json
+import random
+import time
 from datetime import datetime
 
+# Inicializar id
+id = 0
 
-# Datos dummy para los archivos JSON
-timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-data0 = {"id": 0, "state": "ON", "timestamp": timestamp}
-data1 = {"id": 1, "state": "OFF", "timestamp": timestamp}
+# Bucle indefinido
+while True:
+  # Datos dummy para los archivos JSON
+  timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+  data0 = {"data":{"id": id, "state": random.randint(0, 1), "timestamp": timestamp}}
+  data1 = {"data":{"id": id, "state": random.randint(0, 1), "timestamp": timestamp}}
 
-timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-data2 = {"id": 2, "state": "OFF", "timestamp": timestamp}
-data3 = {"id": 3, "state": "ON", "timestamp": timestamp}
+  # Escribir los datos en los archivos JSON
+  with open('/usr/share/filebeat/logs/dummy_0.json', 'a') as f:
+      f.write(json.dumps(data0) + '\n')
 
-# Escribir los datos en los archivos JSON
-with open('/usr/share/filebeat/logs/dummy_0.json', 'w') as f:
-    f.write(json.dumps(data0) + '\n')
-    f.write(json.dumps(data1) + '\n')
+  with open('/usr/share/filebeat/logs/dummy_1.json', 'a') as f:
+      f.write(json.dumps(data1) + '\n')
 
-with open('/usr/share/filebeat/logs/dummy_1.json', 'w') as f:
-    f.write(json.dumps(data2) + '\n')
-    f.write(json.dumps(data3) + '\n')
+  # Incrementar id
+  id += 1
+  
+  # Esperar 10s
+  time.sleep(10)
